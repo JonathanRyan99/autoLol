b35e41d888dc0b4497e314e0795a7e015beec3f7
Jonathan Ryan <jonny788@live.co.uk>
2020-11-28 20:36:37 +0000

    added main, acceptbutton, interface images

--- acceptButton.py
+++ acceptButton.py
@@ -1,2 +1,14 @@
 #accept button
+import pyautogui
 
+
+def press_AcceptButton():
+	try:
+		pyautogui.click('interface/accept.PNG')
+		print("accept pressed")
+	except:
+		print("AcceptButton not found")
+		return False
+	return True
+
+press_AcceptButton()
